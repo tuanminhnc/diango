8011458c1e282006f6b493010fd4099c5857ba5a
Nguyen Cong Tuan Minh <nguyencongtuanminh@gmail.com>
2023-06-20 19:21:13 +0000

    make migrate db

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,6 +89,9 @@ DATABASES = {
     }
 }
 
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'fixtures/')
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from polls.models import Question, Choise
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question_text', 'pub_date']
+
+class ChoiseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question',  'choise_text', 'vote']
+
+    def question(self, obj):
+        return  obj.question.question_text
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choise, ChoiseAdmin)
